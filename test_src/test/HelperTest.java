a8131f0d698a504f5f25a0452950bf7c7ba22b80
Will Henry <sunrize920@gmail.com>
2013-04-22 16:06:09 -0600

    added tests

--- test_src/test/HelperTest.java
+++ test_src/test/HelperTest.java
@@ -37,6 +37,7 @@ public class HelperTest extends TestCase{
 
 	/**
 	* tests that the entire course list is returned from the database
+	* @author Will Henry
 	*/
 	public void testGetCourseList(){
 		//create helper
@@ -46,8 +47,74 @@ public class HelperTest extends TestCase{
 		//check the size
 		assertEquals("Course size", 315, courses.size());
 		for(Course c: courses){
-			assertEquals("Course Id is 1", 1, c.getReqFulfilled());
+			assertEquals("Requirement Id is 1", 1, c.getReqFulfilled());
 		}
+		//return the list of Courses from the database with requirement id 7
+		courses = helper.getCourseList(7);
+		for(Course c: courses){
+			assertEquals("Requirement Id is 7", 7, c.getReqFulfilled());
+		}
+	}
+	/**
+	* test that the entire Section list is returned for a particular course
+	* @author Will Henry
+	*/
+	public void testGetSectionList(){
+		//create helper
+		Helper helper = new Helper();
+		//return the list of Sections from the database with section id 1
+		ArrayList<Section> sections = helper.getSectionList(1);
+		for(Section s:sections){
+			assertEquals("Course Id is 1", 1, s.getCourseId());
+		}
+		//test for section id 10
+		sections = helper.getSectionList(10);
+		for(Section s:sections){
+			assertEquals("Course Id is 10", 10, s.getCourseId());
+		}	
+	}
+	/**
+	* test that the meeting list is returned for a particular section
+	* @author Will Henry
+	*/
+	public void testGetMeetingList(){
+		//create helper
+		Helper helper = new Helper();
+		//return the list of Meetings from the database with section id 1
+		ArrayList<Meeting> meetings = helper.getMeetingList(1);
+		for(Meeting m: meetings){
+			assertEquals("Section Id is 1", 1, m.getSectionId());
+		}
+		meetings = helper.getMeetingList(7);
+		for(Meeting m: meetings){
+			assertEquals("Section Id is 7", 7, m.getSectionId());
+		}		
+	}
+	/**
+	* tests that a section is added to the database
+	* @author Will Henry
+	*/
+	public void testAddSection(){
+		//create helper
+		Helper helper = new Helper();
+		//get current section list
+		ArrayList<Section> sections = helper.getSectionList(1);
+		int size = sections.size();
+		//add a Section
+		helper.addSection("123456", "4", "Dan Everett", 1);
+		sections = helper.getSectionList(1);
+		assertEquals("Size of section list increased by 1", size+1, sections.size());
+		//add a second section
+		helper.addSection("123455", "5", "Dan Everett", 1);
+		sections = helper.getSectionList(1);
+		assertEquals("Size of section list increased by 2", size+2, sections.size());
+		//delete them
+		helper.deleteSection(sections.get(size).getId());
+		helper.deleteSection(sections.get(size+1).getId());
+		sections = helper.getSectionList(1);
+		assertEquals("both sections deleted", size, sections.size());
+
+
 
 	}
 }	
