c95369c31184226a3d9302252efd05986db298c2
David Sawyer <davidsawyer10@gmail.com>
2013-04-22 14:18:22 -0400

    implemented core Controller functionality

--- src/controller/CoursePickerController.java
+++ src/controller/CoursePickerController.java
@@ -1,11 +1,20 @@
 package controller;
 
+import helper.Helper;
+
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Course;
+import model.Section;
 
 /**
  * Controller servlet for the course picker application
@@ -25,7 +34,33 @@ public class CoursePickerController extends HttpServlet {
 	 * welcome page
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		ServletContext ctx = this.getServletContext();
+		RequestDispatcher dispatcher;
+		
+		Helper helper = new Helper();
+		
+		String requirementId = request.getParameter("requirementId");
+		String courseId = request.getParameter("courseId");
+		
+		if (requirementId != null) {
+			try {
+				ArrayList<Course> courses = helper.getCourseList(Integer.parseInt(requirementId));
+				request.setAttribute("courses", courses);
+			} catch (Exception e) {
+				System.out.println("Could not parse requirementId.");
+			}
+		}
+		else if (courseId != null) {
+			try {				
+			ArrayList<Section> sections = helper.getSectionList(Integer.parseInt(courseId));
+			request.setAttribute("sections", sections);
+			} catch (Exception e) {
+				System.out.println("Could not parse requirementId.");
+			}
+		}
+		
+		dispatcher = ctx.getRequestDispatcher("/index.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
